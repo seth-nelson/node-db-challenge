b9e0b43cd1a38135e6fae67357cce297a306fb0d
Seth Nelson <seth.nelson@outlook.com>
2020-05-31 19:24:55 -0700

    added the migration table formats in all 3 files

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ server.use('/api/projects', projectRouter);
 
 // Page shows h2 when properly running
 server.get('/', (req, res) => {
-    res.send(`<h2>Server is responding.</h2>`);
+    res.send(`<h2>Server is responding and running.</h2>`);
 });
 
 module.exports = server;
---
+++ router/projectRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('../model/projectModel.js');
+const router = express.Router();
+
+// Endpoints
+
+// GET projects
+// use db getProjects
+
+// POST project
+// use db addNewProject
+
+// GET tasks
+// use db getTasks
+
+// POST task
+// use db addNewTask
+
+// GET resources
+// use db getResources
+
+// POST resource
+// use db addNewResource
+
+
+module.exports = router;
